0c315d4e7136afa980ab5a0eb3110bd6582ae920
Daniel Leal <dlleal@uc.cl>
2022-03-06 03:49:50 -0300

    Add some base styling to the button

--- src/components/GButton/GButton.vue
+++ src/components/GButton/GButton.vue
@@ -1,9 +1,24 @@
 <script setup lang="ts">
+import { computed } from 'vue';
+
 const $props = defineProps<{
   text: string,
 }>();
+
+const $emit = defineEmits<{(e: 'click'): void}>();
+
+const classes = computed(() => ['g-button']);
 </script>
 
 <template>
-  {{ $props.text }}
+  <button
+    :class="classes"
+    @click="$emit('click')"
+  >
+    {{ $props.text }}
+  </button>
 </template>
+
+<style lang="scss" scoped>
+@use './g-button.scss';
+</style>
