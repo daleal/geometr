eab481ca58d3ed5312ba6f5333ed091e627f53da
Daniel Leal <cosaleal@gmail.com>
2023-02-10 21:02:04 -0300

    chore: add stylelint to the project

---
+++ stylelint.config.cjs
@@ -0,0 +1,30 @@
+module.exports = {
+  extends: ['stylelint-config-standard'],
+  plugins: [
+    'stylelint-scss',
+  ],
+  overrides: [
+    {
+      files: ['**/*.vue'],
+      customSyntax: 'postcss-html',
+    },
+    {
+      files: ['**/*.scss'],
+      customSyntax: 'postcss-scss',
+    },
+  ],
+  ignoreFiles: ['./src/styles/generic/_reset.scss'],
+  rules: {
+    'at-rule-empty-line-before': [
+      'always',
+      {
+        except: ['after-same-name'],
+        ignore: ['after-comment'],
+        ignoreAtRules: ['else'],
+      },
+    ],
+    'at-rule-no-unknown': null,
+    'scss/at-rule-no-unknown': true,
+    'selector-class-pattern': null,
+  },
+};
