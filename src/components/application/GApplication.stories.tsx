6287b1599c608744c91218e56e89faae24fb32f1
Daniel Leal <cosaleal@gmail.com>
2023-02-11 00:54:13 -0300

    feat: create base application container

---
+++ src/components/application/GApplication.stories.tsx
@@ -0,0 +1,26 @@
+import type { Meta, StoryObj } from '@storybook/vue3';
+import { GApplication } from './index';
+
+// Auxiliar Types
+type Story = StoryObj<typeof GApplication>;
+
+// Meta
+const meta: Meta<typeof GApplication> = {
+  title: 'components/GApplication',
+  component: GApplication,
+  parameters: { controls: { sort: 'requiredFirst' } },
+};
+
+export default meta;
+
+// Exported Stories
+export const ApplicationBase: Story = {
+  parameters: {
+    layout: 'fullscreen',
+  },
+  render: (args) => (
+    <GApplication {...args} style={{ padding: '1rem' }} >
+      This application has content
+    </GApplication>
+  ),
+};
