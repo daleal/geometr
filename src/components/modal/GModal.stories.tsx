9348d4797515f3a4220a20253bf52310e42a33e7
Daniel Leal <cosaleal@gmail.com>
2023-07-21 21:57:29 -0400

    feat: restrict modal width

--- src/components/modal/GModal.stories.tsx
+++ src/components/modal/GModal.stories.tsx
@@ -45,6 +45,22 @@ export const ModalWithContent = {
   }),
 } satisfies Story;
 
+export const ModalWithLargeContent = {
+  ...BaseStory({
+    title: () => <>Some Title</>,
+    default: () => <>
+      This is a modal! Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+      Mauris imperdiet porttitor odio vel viverra. Sed cursus gravida enim et mattis.
+      Vivamus eu velit tempus, ullamcorper metus vitae, iaculis libero.
+      Etiam ultrices gravida elit, ac auctor diam tristique at.
+      Ut et ex ac ipsum convallis sodales non nec erat.
+      Duis gravida erat id lectus tristique rutrum.
+      Praesent vitae faucibus tellus.
+      Mauris eu mauris egestas, consectetur lectus et, semper dolor.
+    </>,
+  }),
+} satisfies Story;
+
 export const PersistentModalWithContent = {
   ...BaseStory({
     default: () => <>This is a modal!</>,
