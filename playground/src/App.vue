a0b189edcfa257bfd803e21a5c0efaae4919b1c7
Daniel Leal <cosaleal@gmail.com>
2023-07-23 22:25:24 -0400

    fix: ignore playground file not being found for importing

--- playground/src/App.vue
+++ playground/src/App.vue
@@ -3,7 +3,7 @@ import { GApplication } from '@/components/main';
 
 // eslint-disable-next-line @typescript-eslint/ban-ts-comment
 // @ts-ignore: This will always fail on a clean repository
-import GPlayground from './GPlayground.vue';
+import GPlayground from './GPlayground.vue'; // eslint-disable-line import/no-unresolved
 </script>
 
 <template>
